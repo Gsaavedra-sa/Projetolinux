da4280a6e29146beb73891a85aefdd159b2286b7
gsaavedra-sa <gabrielsaito19@gmail.com>
2020-03-06 20:46:57 +0000

    criando tela

--- script.sh
+++ script.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 #autor: gabriel saito
 #AWS running instance EC2
+
+
+
+echo "Qual o numero do serviço que voce deseja alterar?"
+echo "1)ssh"
+echo "2)apache"
+echo "3)dhcp"
+read serv;
+
+
